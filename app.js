c7f0f154d50d43a895a77e03159b06934b9f5d48
KevinXu17 <xuzhaozhishu@gmail.com>
2021-05-11 11:04:39 -0700

    #WHAT HAVE DONE# ALL routers and views set up

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-const Campgroud = require('./models/campgroud')
+const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 
 // app
@@ -30,25 +31,60 @@ db.once("open", ()=> {
 app.set('view engine', 'ejs')
 // path
 app.set('views', path.join(__dirname, "views"))
+// parser
+app.use(express.urlencoded({extended: true}))
+// method-override
+app.use(methodOverride('_method'))
 
+// router
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campgroud ({
-//         title: "camp1",
-//         price: "20",
-//         description: "camp1 is good",
-//         location: "Vancouver"
-//     })
-//     try {
-//         const c = await camp.save();
-//         console.log(c)
-//     } catch (e) {
-//         console.log("Failed to save the new camp")
-//     }
-// })
+app.get('/campgrounds', async (req, res) => {
+    try {
+        const campgrounds = await Campground.find({});
+        res.render('campgrounds/index', {campgrounds})
+    } catch (e) {
+        console.log("Failed to load data")
+        console.log(e)
+    }
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+ 
+app.get('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findById({_id: id});
+    res.render('campgrounds/show', {campground})
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findById({_id: id})
+    res.render("campgrounds/edit", {campground})
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    await Campground.updateOne({_id: req.params.id}, {...req.body.campground})
+    res.redirect(`/campgrounds/${req.params.id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    await Campground.deleteOne({_id:req.params.id})
+    res.redirect("/campgrounds")
+})
+
+
+
 
 app.listen(port, ()=>{
     console.log(`The server is set up at ${port}`)
